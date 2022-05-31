9635736623cffe0c02cf79479b8ffeb7bdb8b0f9
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-31 11:14:58 -0400

    Created local extension of JpaRepository (which going back a few extension is linked to crud)

---
+++ src/main/java/me/rallings/springbootregistrationapp/UserRepository.java
@@ -0,0 +1,7 @@
+package me.rallings.springbootregistrationapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
