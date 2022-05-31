a2f1df14920aa846ecfa21820fab35d2dfaf70eb
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-31 11:03:02 -0400

    Initial commit

---
+++ src/test/java/me/rallings/springbootregistrationapp/SpringbootRegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.springbootregistrationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRegistrationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
